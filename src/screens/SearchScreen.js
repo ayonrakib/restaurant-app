e31382bf4be98f3d937f3c0502df2ef1deb3c153
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-02-24 01:19:17 -0600

    first commit

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function SearchScreen(){
+    return (
+        <View>
+            <Text>
+                Screen
+            </Text>
+        </View>
+    )
+}

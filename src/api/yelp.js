16c62a02eeabb8c1007a2b47f97e9f3254ab0dfd
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-02-28 17:07:38 -0600

    yelp api

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,2 +1,10 @@
 import axios from "axios";
 
+export default axios.create({
+    baseURL: "https://api.yelp.com/v3/businesses/search",
+    method: "GET",
+    data: "",
+    headers: {
+        Authorization: 'Bearer Wp4EMYfdTk-VeSx2W8Q4wl4_sx3vPZIfFU7aEjsWlQJzhJiLNwY2p4LUjyytScHZ0jg9hwq9GO68CVTbu4jQRkU1lL3srv7hcvmok1GWXruym7w5vyMkOPFeiSUXYnYx'
+    }
+})

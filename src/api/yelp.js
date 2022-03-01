b82b83a86793facc32d4cbd618a874de019e64dd
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-03-01 14:00:37 -0600

    .env in react native

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,10 +1,13 @@
 import axios from "axios";
+import {YELP_API_KEY, YELP_CLIENT_ID} from "@env";
+
+console.log("env variables are: ", YELP_API_KEY)
 
 export default axios.create({
     baseURL: "https://api.yelp.com/v3/businesses/search",
     method: "GET",
     data: "",
     headers: {
-        Authorization: 'Bearer Wp4EMYfdTk-VeSx2W8Q4wl4_sx3vPZIfFU7aEjsWlQJzhJiLNwY2p4LUjyytScHZ0jg9hwq9GO68CVTbu4jQRkU1lL3srv7hcvmok1GWXruym7w5vyMkOPFeiSUXYnYx'
+        Authorization: `Bearer ${YELP_API_KEY}`
     }
 })

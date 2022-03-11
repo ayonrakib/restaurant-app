ab56f804216b888112079429e5fba64167da902b
DESKTOP-A22BN7T\ayonr <ayonrakib@gmail.com>
2022-03-11 00:23:48 -0600

    show restaurants on screen

--- src/hooks/useResults.js
+++ src/hooks/useResults.js
@@ -3,30 +3,50 @@ import yelp from "../api/yelp";
 
 export default () => {
     const [results, setResults] = useState([]);
-    const [errorInSearchingRestaurants, setErrorInSearchingRestaurants] = useState(undefined)
-    console.log("results state is: ",results)
+    const [errorInSearchingRestaurants, setErrorInSearchingRestaurants] = useState("")
+    // console.log("results state is: ",results)
+    // if (results != []){
+    //     console.log("name of the first res is: ",results[0].name)
+    // }
+    
 
-    const searchApi = async (searchTerm) => {
-        console.log("came in search api method!")
+    const searchApi = async (searchTerm, callBack) => {
+        // console.log("came in search api method!")
         try {
             const response = await yelp.get('/search', {
                 params: {
-                    limit: 10,
+                    limit: 3,
                     term: searchTerm,
                     location: 'austin'
                 }
             });
-            setErrorInSearchingRestaurants("")
-            setResults(response.data.businesses)
+            // console.log("response from yelp is: ",response.data)
+            callBack(response.data.businesses)
+             
         } catch (error) {
             setErrorInSearchingRestaurants("Could not search restaurants!")
         }
 
     }
 
-    useEffect(() => {
-        searchApi('noodles')
-    }, [])
 
+    const showCheapRestaurants = () => {
+        if (results === []){
+            return null
+        }
+        console.log("results in showCheapRestaurants method is: ",results.length)
+        for(let index in results){
+            // console.log("restaurant name is: ",results[index])
+        }
+    }
+
+    useEffect(() => {
+        searchApi('noodles');
+        if(results != []){
+            showCheapRestaurants(); 
+        }
+    }, [])
+    
+    // console.log("results before returning is: ",results)
     return [errorInSearchingRestaurants, results, searchApi]
 }
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,20 +5,48 @@ import useResults from "../hooks/useResults";
 import Results from "../components/Results";
 
 export default function SearchScreen(){
-
+    console.log("came in searchscreen component!")
     const [term, setTerm] = useState('')
     const [errorInSearchingRestaurants, results, searchApi] = useResults();
+    const [restaurants, SetRestaurants] = useState([]);
 
     const filterResultsByPrice = (price) => {
         // price === "$" || "$$" || "$$$"
     }
- 
+
+    // console.log("updated restaurant state in searchscreen method: ",restaurant)
+
+    // 1. restaurants = list of restaurant objects
+    // 2. restaurantJSXList empty list
+    // 3. restaurants er proti ta restaurant object er jonno:
+    //      3.1. restaurantJSXList e push restaurant object er name key er value string akare
+    // 4. restaurantJSXList ke ekta View tag diye enclose korbo
+    // 5. restaurantJSXList er value return e boshabo
+    let restaurantJSXList = []
+    for(let index in restaurants){
+        // console.log("name of the current restaurant is: ",restaurants[index].name)
+        restaurantJSXList.push(
+            <View>
+                <Text key={Math.random()}>{restaurants[index].name } {"\n"}</Text> 
+                <Image
+                    style = {{ width: 100, height: 100, borderRadius: 18 }}
+                    source={{
+                    uri: restaurants[index].image_url,
+                    }}
+                />
+            </View>
+        
+        
+        
+        )
+    }
+    // console.log("restaurantJSXList is: ",restaurantJSXList)
     return (
         <View>
             <SearchBar 
                 term = {term} 
                 onTermChange = {setTerm} 
-                onTermSubmit = {() => searchApi(term)}
+                onTermSubmit = {() => searchApi(term, SetRestaurants)}
             />
             <Text>
                 {errorInSearchingRestaurants}
@@ -34,7 +62,10 @@ export default function SearchScreen(){
                 source={{
                 uri: 'https://s3-media1.fl.yelpcdn.com/bphoto/vp536ivd0pgr9AuYjqvfUw/o.jpg',
                 }}
-            />           
+            />
+            <Text>
+                 {restaurantJSXList}
+            </Text>  
         </View>
     )
 }
